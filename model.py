896a778360c562e176b99ac7e3c01625366cf676
Adithya Vardan M <115080544+AdithyaVardan1@users.noreply.github.com>
2024-04-02 17:12:06 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+quantized_model_dir = "AVMLegend/Quantised-Phi-2"
+
+model = AutoModelForCausalLM.from_pretrained(quantized_model_dir, device_map="auto", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(quantized_model_dir)
+
+prompt = "Explain logistic regression"
+inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
+
+outputs = model.generate(**inputs, max_new_tokens=200)
+text = tokenizer.batch_decode(outputs)[0]
+
+last_stop_index = text.rfind(".")
+if last_stop_index != -1:
+  text = text[:last_stop_index + 1]
+print(text)
